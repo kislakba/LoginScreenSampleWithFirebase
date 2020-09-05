01a59fb60461971366ac81e199083b7b181606ed
baris <baris.kislak@stu.fsm.edu.tr>
2020-09-05 10:14:06 +0300

    first commit

---
+++ app/src/main/java/com/example/login_screen_sample_with_firebase/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.login_screen_sample_with_firebase.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.login_screen_sample_with_firebase.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
