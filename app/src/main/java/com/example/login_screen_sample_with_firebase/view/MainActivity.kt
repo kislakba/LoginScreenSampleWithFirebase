30a1b49840051f9f50a016dcf6c57a7a43919682
baris <baris.kislak@stu.fsm.edu.tr>
2020-09-18 12:10:05 +0300

    ---Sample application has been completed--- 1- You can just connect the application with firebase for use the application 2- You can change the image of the application XML comment lines will help you 3- For next step I will update readme file

--- app/src/main/java/com/example/login_screen_sample_with_firebase/view/MainActivity.kt
+++ app/src/main/java/com/example/login_screen_sample_with_firebase/view/MainActivity.kt
@@ -8,7 +8,7 @@ import com.example.login_screen_sample_with_firebase.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_login)
+        setContentView(R.layout.activity_main)
     }
 
 }
